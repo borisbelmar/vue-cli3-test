e65f323a02d2f544982eda80c15920b8da86f518
Boris Belmar <borisbelmarm@gmail.com>
2019-07-15 14:36:34 -0400

    Trabajando con Vuex

---
+++ src/components/Lista.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li v-for="(fruta, index) of frutasOrdenadas" :key="fruta.id" @click="aumentar(index)" class="list-group-item d-flex justify-content-between align-items-center">
+                {{index}} - {{fruta.nombre}}
+                <span class="badge badge-primary badge-pill">{{fruta.cantidad}}</span>
+            </li>
+        </ul>
+        <button class="btn btn-danger btn-lg btn-block" @click="reiniciar">Reiniciar</button>
+    </div>
+</template>
+
+<script>
+    import { mapState, mapMutations } from 'vuex';
+
+    export default {
+        name: 'Lista',
+        computed: {
+            ...mapState(['frutas']),
+            frutasOrdenadas() {
+                // Ordenar de mayor a menor
+                return this.frutas.sort( (a, b) => b.cantidad - a.cantidad )
+            }
+        },
+        methods:{
+            ...mapMutations(['aumentar', 'reiniciar'])
+        }
+    }
+</script>
